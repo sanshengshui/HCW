8bd48545b212df6c638d20b8e56b69d53a716be3
sanshengshui <lovewsic@gmail.com>
2017-05-18 16:57:56 +0800

    remove useless code and modify some comments

--- src/main/java/com/haro/netty/util/DataUtils.java
+++ src/main/java/com/haro/netty/util/DataUtils.java
@@ -15,8 +15,8 @@ import org.springframework.util.StringUtils;
  * 
  * 类描述：时间操作定义类
  * 
- * @author:  张代浩
- * @date： 日期：2012-12-8 时间：下午12:15:03
+ * @author:  穆书伟
+ * @date： 日期：2017/5/18 时间：下午16：55
  * @version 1.0
  */
 public class DataUtils extends PropertyEditorSupport {
